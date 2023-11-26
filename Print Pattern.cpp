c6834b3dda2a158f65a051147743761556969b71
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-11-26 18:54:42 +0530

    Update Print Pattern.cpp

--- Print Pattern.cpp
+++ Print Pattern.cpp
@@ -2,7 +2,7 @@ class Solution{
 public:
     void solve(vector<int> &list, int n, bool flag, int k) {
         list.push_back(n);
-        if (flag && n > 0) solve(list, n - 5, flag, k);
+        if (flag && n > 0) solve(list, n - 5, flag, k); 
         if(n<=0) solve(list,n+5,false,k);
         if (!flag && n > 0 && n < k) solve(list, n + 5, false, k);
         else return;
