9ccd5dc26b3aca9892d28c91ddf6e6f5b18f8085
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-12-06 21:09:36 +0530

    Update How Many X's?.cpp

--- How Many X's?.cpp
+++ How Many X's?.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int countX(int L, int R, int X) {
         // code here
-        int count =0;
+        int count =0; 
         for(int i =L+1;i<R ;i++){
             int num=i;
             while(num!=0){
