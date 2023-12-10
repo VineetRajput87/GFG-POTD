e9625814b43480feee50b3fc6c544d7d868102fd
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-12-10 12:16:15 +0530

    Update Subarray with 0 sum.cpp

--- Subarray with 0 sum.cpp
+++ Subarray with 0 sum.cpp
@@ -4,7 +4,7 @@ class Solution{
     //Function to check whether there is a subarray present with 0-sum or not.
     bool subArrayExists(int arr[], int n)
     {
-       unordered_set<int> set;
+       unordered_set<int> set; 
         int sum = 0;
         for (int i = 0; i < n; i++) {
             set.insert(sum);
