c1ece2784e0a9a103f21a53762d993cbe2524851
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-12-10 12:16:03 +0530

    Create Subarray with 0 sum.cpp

---
+++ Subarray with 0 sum.cpp
@@ -0,0 +1,18 @@
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+       unordered_set<int> set;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            set.insert(sum);
+            sum = sum + arr[i];
+
+            if (set.find(sum) != set.end())
+                return true;
+        }
+        return false;
+    }
+};
