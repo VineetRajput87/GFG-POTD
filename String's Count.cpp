0902a386ff51c6030e93a0fac6d2fe2e3ab18bda
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-12-16 21:56:01 +0530

    Update String's Count.cpp

--- String's Count.cpp
+++ String's Count.cpp
@@ -3,7 +3,7 @@ long long int countStr(long long int n){
 	    // all a       --> 1 
 	    // 1b and a    --> n
 	    // 1c and a    --> n
-	    // 2c and a    --> n*(n-1)/2
+	    // 2c and a    --> n*(n-1)/2 
 	    // 1b,1c and a --> n*(n-1)
 	    // 1b,2c and a --> n(n-1)(n-2)/2
 	    
