46e7c78903c11da5b66488569de4f72de9991a71
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-12-17 20:11:01 +0530

    Update Max Sum without Adjacents.cpp

--- Max Sum without Adjacents.cpp
+++ Max Sum without Adjacents.cpp
@@ -4,7 +4,7 @@ public:
 	int findMaxSum(int *arr, int n) { 
 	    // code here
 	    if(n==1) return arr[0];
-	    
+	     
 	    for(int i=2; i<n; i++){
 	        arr[i]+=arr[i-2];
 	        arr[i-1] = max(arr[i-1],arr[i-2]);
