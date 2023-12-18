fdeba7867aab217304d1f0717f8bdefde2652a50
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-12-18 11:36:51 +0530

    Update Game of XOR.cpp

--- Game of XOR.cpp
+++ Game of XOR.cpp
@@ -6,7 +6,7 @@ class Solution {
         
         for(int i=0; i<N; i++){
             int y = (i+1)*(N-i);
-            if(y%2==1) x=x^A[i];
+            if(y%2==1) x=x^A[i]; 
         }
         return x;
     }
