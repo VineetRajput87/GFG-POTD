fbc6678ce9938ff6896780e6060e4d05f01ea78c
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-12-20 17:25:04 +0530

    Update Modified Game of Nim.cpp

--- Modified Game of Nim.cpp
+++ Modified Game of Nim.cpp
@@ -2,7 +2,7 @@ class Solution{
 public:
     int findWinner(int n, int A[]){
         // code here
-        if(n%2==0) return 1;
+        if(n%2==0) return 1; 
         int val = 0;
         for(int i=0; i<n; i++) val = val^A[i];
         if(val==0) return 1;
