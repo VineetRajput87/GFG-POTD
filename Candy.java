387552f8e88dd1f8af9a9e4631cbb25d7fb42914
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-12-21 22:43:10 +0530

    Update Candy.java

--- Candy.java
+++ Candy.java
@@ -4,7 +4,7 @@ class Solution {
         int arr[] = new int[n];
         arr[n-1] = 1;
         for(int i=n-2; i>=0; i--){
-            if(rat[i]>rat[i+1]) arr[i]=arr[i+1]+1;
+            if(rat[i]>rat[i+1]) arr[i]=arr[i+1]+1; 
             else arr[i] = 1;
         }
         
