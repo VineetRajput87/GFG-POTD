31342c9c0a83ad12068f2746eea2ba422e035f9a
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-12-27 20:45:20 +0530

    Update Anti Diagonal Traversal of Matrix.cpp

--- Anti Diagonal Traversal of Matrix.cpp
+++ Anti Diagonal Traversal of Matrix.cpp
@@ -4,7 +4,7 @@ public:
     vector<int> antiDiagonalPattern(vector<vector<int>>& matrix) {
         int n = matrix.size(), k = 0, x = 0;
         vector<int> arr(n * n);
-
+ 
         for (int i = 0; i < n; i++) {
             k = 0;
             for (int j = i; j >= 0; j--) arr[x++] = matrix[k++][j];
