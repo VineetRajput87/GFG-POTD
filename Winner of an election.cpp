d1f961d24e6ab22ab3d6724950995139eb9dbab0
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-12-30 21:11:56 +0530

    Update Winner of an election.cpp

--- Winner of an election.cpp
+++ Winner of an election.cpp
@@ -6,7 +6,7 @@ class Solution{
         unordered_map<string, int> mp;
         for(i=0;i<n;i++) mp[a[i]]++;
         for(auto x: mp) {
-            maxi=max(maxi, x.second);
+            maxi=max(maxi, x.second); 
         }
         for(auto x: mp) {
             if(x.second==maxi) v.push_back(x.first);
