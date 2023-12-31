6237054f3bd0eb4bd7ce134f1724b43c464d268d
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-12-31 23:53:05 +0530

    Update New Year Resolution.cpp

--- New Year Resolution.cpp
+++ New Year Resolution.cpp
@@ -3,7 +3,7 @@ class Solution {
     int solve(int i,int N,int coins[],int sum,vector<vector<int>> &dp){
         if(sum==2024) return 1;
         if(sum!=0 and (sum%20==0 || sum%24==0)){
-            return 1;
+            return 1; 
         }
         if(i>=N){
             return 0;
