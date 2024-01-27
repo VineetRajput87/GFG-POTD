4836ac7c308c57f82f937ca3eacd491cedeac66b
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2024-01-27 22:16:28 +0530

    Update Brackets in Matrix Chain Multiplication.cpp

--- Brackets in Matrix Chain Multiplication.cpp
+++ Brackets in Matrix Chain Multiplication.cpp
@@ -7,7 +7,7 @@ public:
         for (int L = 2; L < n; L++) {
             for (int i = 1; i < n - L + 1; i++) {
                 int j = i + L - 1;
-                m[i][j] = INT_MAX;
+                m[i][j] = INT_MAX; 
                 for (int k = i; k < j; k++) {
                     int cost = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
                     if (cost < m[i][j]) {
