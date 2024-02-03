9750119de785a6ac3a35d8dbcbd808096d7b863d
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2024-02-03 19:03:13 +0530

    Update Decimal Equivalent of Binary Linked List.cpp

--- Decimal Equivalent of Binary Linked List.cpp
+++ Decimal Equivalent of Binary Linked List.cpp
@@ -1,7 +1,7 @@
 class Solution
 {
     public:
-        // Should return decimal equivalent modulo 1000000007 of binary linked list 
+        // Should return decimal equivalent modulo 1000000007 of binary linked list  
         long long unsigned int decimalValue(Node *head)
         {
             long long ans = 0;
