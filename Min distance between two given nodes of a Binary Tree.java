418eaad2c9f00cf4aa3d4f0eb674ba6ae90076bf
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2024-02-07 19:00:57 +0530

    Update Min distance between two given nodes of a Binary Tree.java

--- Min distance between two given nodes of a Binary Tree.java
+++ Min distance between two given nodes of a Binary Tree.java
@@ -2,7 +2,6 @@ class GfG {
     int findDist(Node root, int a, int b) {
         Node LCA = lca(root, a, b);
         return findLevel(LCA, a) + findLevel(LCA, b);
-        // return findLevel(LCA, a,0) + findLevel(LCA, b,0); //for dfs
         
     }
     	Node lca(Node root, int n1,int n2){
