98382e53bafc24e4f7d47d5c4a986c02f2f0aa15
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2024-02-09 21:24:10 +0530

    Update Check for Children Sum Property in a Binary Tree.cpp

--- Check for Children Sum Property in a Binary Tree.cpp
+++ Check for Children Sum Property in a Binary Tree.cpp
@@ -8,7 +8,7 @@ class Solution{
             if(!node or (!(node -> left) and !(node -> right))) 
                 return 1;
                 
-            int left = dfs(node -> left);
+            int left = dfs(node -> left); 
             int right = dfs(node -> right);
             
             int sum = (node -> left ? node -> left -> data : 0) + (node -> right ? node -> right -> data : 0);
