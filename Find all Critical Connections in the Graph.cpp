9c516cd2a244df9b2b08b05308b831af64591e81
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2024-02-14 18:27:05 +0530

    Update Find all Critical Connections in the Graph.cpp

--- Find all Critical Connections in the Graph.cpp
+++ Find all Critical Connections in the Graph.cpp
@@ -5,7 +5,7 @@ public:
     vector<vector<int>>ans;
     int time=0;
     void dfs(int u, vector<int>&tin, vector<int>&lowest, vector<int>&parent, vector<int>adj[]){ 
-        tin[u] = lowest[u] =time++;
+        tin[u] = lowest[u] =time++; 
         
         for(int i=0;i<adj[u].size();i++){
             int v=adj[u][i];
