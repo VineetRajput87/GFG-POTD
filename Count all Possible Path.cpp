4bc85057c0fa29f9f0baf32dd4c7b2a3daab6311
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2024-02-15 19:28:46 +0530

    Update Count all Possible Path.cpp

--- Count all Possible Path.cpp
+++ Count all Possible Path.cpp
@@ -4,7 +4,7 @@ public:
 	    // Code here
 	    int n=paths.size();
 	    for(int i=0;i<n;i++){
-	        int count=0;
+	        int count=0; 
 	        for(int j=0;j<n;j++){
 	            if(paths[i][j]==1){
 	                count++;
