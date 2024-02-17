8ced05ed306023d40962850c7862f88a561aa606
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2024-02-17 20:29:54 +0530

    Update Does array represent Heap.cpp

--- Does array represent Heap.cpp
+++ Does array represent Heap.cpp
@@ -4,7 +4,7 @@ class Solution{
     {
         // Your code goes here
         int j = 1;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){ 
             if(j<n and arr[j]>arr[i])return false;
             if(j+1<n and arr[j+1]>arr[i])return false;
             j+=2;
