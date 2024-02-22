b4741da654e3a6005a22a93c2369262b71acb6df
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2024-02-22 23:08:37 +0530

    Update Distinct occurrences.cpp

--- Distinct occurrences.cpp
+++ Distinct occurrences.cpp
@@ -2,7 +2,7 @@ class Solution
 {
     public:
     int mod = 1e9+7;
-    int help(string s,string t,int i,int j,vector<vector<int>> &dp){
+    int help(string s,string t,int i,int j,vector<vector<int>> &dp){ 
         if(j==t.length())return 1;
         if(i==s.length())return 0;
         if(dp[i][j]!=-1)return dp[i][j];
