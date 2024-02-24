1b587243805a675dacace93f3b0ab14a5c2ee1f9
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2024-02-24 21:06:11 +0530

    Update Maximum Sum Problem.cpp

--- Maximum Sum Problem.cpp
+++ Maximum Sum Problem.cpp
@@ -5,7 +5,7 @@ class Solution
             if(n==0 || n==1) return n;
             if(dp[n]!=-1) return dp[n];
             
-            return dp[n]=max(n, solve(n/2,dp)+solve(n/3,dp) + solve(n/4,dp));
+            return dp[n]=max(n, solve(n/2,dp)+solve(n/3,dp) + solve(n/4,dp)); 
         }
         
         int maxSum(int n)
